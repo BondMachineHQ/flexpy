a4854f59a53dac2ae748470929787bc5bbcbecf2
Mirko Mariotti <mirko@mirkomariotti.it>
2024-12-13 18:30:27 +0100

    :sparkles: (flexpy) hls strructure

--- flexpy.py
+++ flexpy.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
 
 from sympy import *
-from qiskit import QuantumCircuit
-from qiskit.circuit import Parameter, ParameterVector
-from qiskit_symb.quantum_info import Operator
-from IPython import display
+# from qiskit import QuantumCircuit
+# from qiskit.circuit import Parameter, ParameterVector
+# from qiskit_symb.quantum_info import Operator
+# from IPython import display
 
-y = Parameter('y')
-p = ParameterVector('p', length=2)
+# y = Parameter('y')
+# p = ParameterVector('p', length=2)
 
-pqc = QuantumCircuit(2)
-pqc.ry(y, 0)
-pqc.cx(0, 1)
-pqc.u(0, *p, 1)
+# pqc = QuantumCircuit(2)
+# pqc.ry(y, 0)
+# pqc.cx(0, 1)
+# pqc.u(0, *p, 1)
 
-pqc.draw('mpl')
+# pqc.draw('mpl')
 
-op = Operator(pqc)
-sm = op.to_sympy()
+# op = Operator(pqc)
+# sm = op.to_sympy()
 
-test = sm[3,0]
-# x = Symbol('x')
-# l = Symbol('l')
-# test = cos(1+x)+l
+# test = sm[3,0]
+x = Symbol('x')
+l = Symbol('l')
+test = (1+x)+l
 
 class flexpyEngine:
 	def __init__(self, symexpr=None):
@@ -59,14 +59,45 @@ _start:
 		self.inputs = []
 		self.index = 0
 	def to_basm(self):
+		self.index = 0
+		self.inputs = []
 		self.basmEngine(self.expr)
 		return self.basm
 	def to_hls(self):
+		self.index = 0
+		self.inputs = []
 		self.hlsEngine(self.expr)
 		return self.hls
 	def hlsEngine(self, expr):
-		self.hls += "Not implemented yet"
-		return
+		self.index+=1
+		myIndex = int(self.index)
+		out = "node_"+str(myIndex)
+		if not myIndex == 1:
+			if len(expr.args) == 0:
+				# This is an input node, create a link using the index among all the inputs, even if inputs grow the index will be unique
+				if not str(expr) in self.inputs:
+					self.inputs.append(str(expr))
+				inIdx = self.inputs.index(str(expr))
+				self.hls += "TERMINAL\n"
+			else:
+				# Create my side of the link, my caller will create the other side
+				for i in range(len(expr.args)):
+					arg=expr.args[i]
+					inp,idx=self.hlsEngine(arg)
+					self.hls += "\n"
+					# Create link to the called nodeindex
+				self.hls += "\n"
+		else:
+			self.hls += "void func(unsigned int N, ARGS){\n"
+			self.hls += "#pragma hls interface mode=m_axi port=x offset=slave bundle=gmem\n"
+			self.hls += "#pragma hls interface mode=m_axi port=y offset=slave bundle=gmem\n"
+			self.hls += "#pragma hls interface mode=m_axi port=z offset=slave bundle=gmem\n"
+			self.hls += "for(int i = 0; i < N; i++){\n"
+			self.hls += "  #pragma hls pipeline\n"
+			self.hls += "  out[i] = OP;\n"
+			self.hls += " }\n"
+			self.hls += "\n"
+		return out,myIndex
 	def basmEngine(self, expr):
 		self.index+=1
 		myIndex = int(self.index)
